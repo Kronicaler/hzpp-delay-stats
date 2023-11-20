14eda0934404a67d30067b03554d0a93b37b012b
TheKroni <68446074+TheKroni@users.noreply.github.com>
2023-11-20 21:21:35 +0100

    Model progress

--- migrations/20231120164348_initial_migration.up.sql
+++ migrations/20231120164348_initial_migration.up.sql
@@ -11,14 +11,14 @@ CREATE TABLE IF NOT EXISTS routes(
     train_number int NOT NULL,
     source varchar(255) NOT NULL,
     destination varchar(255) NOT NULL,
-    bikes_allowed smallint NOT NULL,
-    wheelchair_accessible smallint NOT NULL,
-    route_type smallint NOT NULL,
-    real_start_time timestamp NULL,
+    bikes_allowed char NOT NULL,
+    wheelchair_accessible char NOT NULL,
+    route_type char NOT NULL,
     expected_start_time timestamp NOT NULL,
-    real_end_time timestamp NULL,
+    real_start_time timestamp NULL,
     expected_end_time timestamp NOT NULL,
-    delay interval NOT NULL
+    real_end_time timestamp NULL,
+    days_of_the_week char -- 0, 1, 2 = monday, tuesday, wednesday
 );
 
 CREATE TABLE IF NOT EXISTS stops(
@@ -27,11 +27,10 @@ CREATE TABLE IF NOT EXISTS stops(
     route_id varchar(255) NOT NULL,
     sequence int NOT NULL,
     code int NOT NULL,
-    real_start_time timestamp NOT NULL,
-    expected_start_time timestamp NOT NULL,
-    real_end_time timestamp NOT NULL,
-    expected_end_time timestamp NOT NULL,
-    delay interval NOT NULL,
+    expected_arrival timestamp NOT NULL,
+    real_arrival timestamp NOT NULL,
+    expected_departure timestamp NOT NULL,
+    real_departure timestamp NOT NULL,
     FOREIGN KEY (station_id) REFERENCES stations(id),
     FOREIGN KEY (route_id) REFERENCES routes(id)
 );
