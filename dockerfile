1ac227a90b5f8a1186e9e0e293cb3c7417b6325e
Kroni <68446074+Kronicaler@users.noreply.github.com>
2024-01-25 18:36:54 +0100

    Refactoring and docker caching

--- dockerfile
+++ dockerfile
@@ -1,27 +1,54 @@
-FROM lukemathwalker/cargo-chef:latest-rust-1 AS chef
+FROM rust:slim-buster AS builder
 WORKDIR /app
 
-FROM chef AS planner
-COPY . .
-RUN cargo chef prepare --recipe-path recipe.json
+RUN apt-get update && apt-get install --no-install-recommends -y pkg-config openssl libssl-dev build-essential wget && rm -rf /var/lib/apt/lists/*;
 
-FROM chef AS builder 
-WORKDIR /app
+ENV SCCACHE_VERSION=0.5.0
 
-RUN apt-get update && apt-get install --no-install-recommends -y cmake && rm -rf /var/lib/apt/lists/*;
+RUN ARCH= && alpineArch="$(dpkg --print-architecture)" \
+      && case "${alpineArch##*-}" in \
+        amd64) \
+          ARCH='x86_64' \
+          ;; \
+        arm64) \
+          ARCH='aarch64' \
+          ;; \
+        *) ;; \
+      esac \
+    && wget -O sccache.tar.gz https://github.com/mozilla/sccache/releases/download/v${SCCACHE_VERSION}/sccache-v${SCCACHE_VERSION}-${ARCH}-unknown-linux-musl.tar.gz \
+    && tar xzf sccache.tar.gz \
+    && mv sccache-v*/sccache /usr/local/bin/sccache \
+    && chmod +x /usr/local/bin/sccache
+
+ENV RUSTC_WRAPPER=/usr/local/bin/sccache
+
+# Pre-compile dependencies
+WORKDIR /build
+
+RUN cargo init --name rust-docker
+
+COPY Cargo.toml Cargo.lock ./
+COPY rust-toolchain.toml ./
+
+RUN --mount=type=cache,target=/root/.cache cargo fetch && \
+    cargo build && \
+    cargo build --release && \
+    rm src/*.rs
+
+# Build the project
+COPY src src
+COPY migrations migrations 
+COPY .sqlx .sqlx
+
 ENV SQLX_OFFLINE=true
-
-COPY --from=planner /app/recipe.json recipe.json
-
-RUN cargo chef cook --release --recipe-path recipe.json
-COPY . .
-RUN cargo build --release
+RUN --mount=type=cache,target=/root/.cache touch src/main.rs && \
+    cargo build --release
 
 FROM debian:bookworm-slim as release
 
 RUN apt-get update && apt-get install --no-install-recommends -y libssl3 ca-certificates && rm -rf /var/lib/apt/lists/*;
 
 WORKDIR /app
-COPY --from=builder /app/target/release/hzpp_delay_stats /app/hzpp_delay_stats
+COPY --link --from=builder /build/target/release/hzpp_delay_stats /app/hzpp_delay_stats
 
 ENTRYPOINT ["/app/hzpp_delay_stats"]
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,23 @@
 #![feature(error_generic_member_access)]
 #![feature(try_blocks)]
 
-use crate::model::hzpp_api_model::HzppRoute;
 use anyhow::Result;
 use background_services::delay_checker::check_delays;
 use background_services::route_fetcher::get_todays_routes;
 use dotenvy::dotenv;
-use itertools::Itertools;
 use model::db_model::RouteDb;
 use opentelemetry::trace::TracerProvider as _;
 use opentelemetry::KeyValue;
-use opentelemetry_otlp::new_exporter;
+use opentelemetry_otlp::{SpanExporterBuilder, TonicExporterBuilder, WithExportConfig};
+use opentelemetry_sdk::runtime::Tokio;
 use opentelemetry_sdk::trace::{Config, TracerProvider};
 use opentelemetry_sdk::Resource;
-use regex::Regex;
 use std::env;
-use std::num::ParseIntError;
-use std::{fs, thread, time::Duration};
-use thiserror::Error;
+use std::time::Duration;
 use tokio::sync::mpsc::channel;
 use tokio::time::sleep;
 use tokio::{select, spawn};
-use tracing::{error, info, instrument, span, warn, Level};
+use tracing::error;
 use tracing_subscriber::filter::LevelFilter;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
@@ -33,48 +29,12 @@ mod model;
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> Result<()> {
     _ = dotenv();
-
-    let span_exporter = new_exporter().tonic().build_span_exporter()?;
-
-    let provider = TracerProvider::builder()
-        .with_simple_exporter(span_exporter)
-        .with_config(
-            Config::default().with_resource(Resource::new(vec![KeyValue::new(
-                "service.name",
-                "HZPP_delay_stats",
-            )])),
-        )
-        .build();
-
-    let tracer = provider.tracer("HZPP_delay_stats");
-
-    let telemetry_layer = tracing_opentelemetry::layer().with_tracer(tracer);
-
-    let env_filter = EnvFilter::builder()
-        .with_default_directive(LevelFilter::INFO.into())
-        .from_env_lossy();
-
-    let stdout_log = tracing_subscriber::fmt::layer().pretty();
-
-    let appender = tracing_appender::rolling::daily("./logs", "hzpp_delay_stats.log");
-    let (non_blocking_appender, _guard) = tracing_appender::non_blocking(appender);
-
-    // A layer that logs events to rolling files.
-    let file_log = tracing_subscriber::fmt::layer()
-        .with_writer(non_blocking_appender)
-        .with_ansi(false)
-        .pretty();
-
-    let _subscriber = Registry::default()
-        .with(telemetry_layer)
-        .with(stdout_log)
-        .with(file_log)
-        .with(env_filter)
-        .init();
+    setup_tracing();
 
     let db_url = env::var("DATABASE_URL").unwrap();
 
     let pool = sqlx::PgPool::connect(&db_url).await.unwrap();
+    sqlx::migrate!("./migrations").run(&pool).await.unwrap();
 
     let (delay_checker_sender, mut delay_checker_receiver) = channel::<Vec<RouteDb>>(32);
 
@@ -108,78 +68,52 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
-#[instrument(err)]
-fn get_delay() -> Result<(), HzppError> {
-    {
-        let span = span!(Level::TRACE, "getting routes");
-        let _enter = span.enter();
+fn setup_tracing() {
+    let provider = TracerProvider::builder()
+        .with_batch_exporter(
+            SpanExporterBuilder::Tonic(
+                TonicExporterBuilder::default()
+                    .with_timeout(Duration::from_millis(500))
+                    .with_endpoint(
+                        dotenvy::var("OTLP_ENDPOINT")
+                            .unwrap_or("http://localhost:4317".to_string()),
+                    ),
+            )
+            .build_span_exporter()
+            .unwrap(),
+            Tokio,
+        )
+        .with_config(
+            Config::default().with_resource(Resource::new(vec![KeyValue::new(
+                "service.name",
+                "HZPP_delay_stats",
+            )])),
+        )
+        .build();
 
-        let routes_file = "documentation/example_responses/routes.json";
-        let routes: Vec<HzppRoute> =
-            serde_json::from_str(&fs::read_to_string(routes_file).map_err(|e| {
-                HzppError::FileReadingError {
-                    source: e,
-                    file: routes_file.to_string(),
-                }
-            })?)?;
+    let tracer = provider.tracer("HZPP_delay_stats");
 
-        info!("Got {} routes", routes.len());
+    let telemetry_layer = tracing_opentelemetry::layer().with_tracer(tracer);
 
-        thread::sleep(Duration::from_millis(25));
-    }
-    {
-        let span = span!(Level::TRACE, "extracting delay from html");
-        let _enter = span.enter();
+    let env_filter = EnvFilter::builder()
+        .with_default_directive(LevelFilter::INFO.into())
+        .from_env_lossy();
 
-        let delay_file = "example_responses/delay.html";
-        let delay_html =
-            fs::read_to_string(delay_file).map_err(|e| HzppError::FileReadingError {
-                source: e,
-                file: delay_file.to_string(),
-            })?;
+    let stdout_log = tracing_subscriber::fmt::layer().pretty();
 
-        if delay_html.contains("Vlak je redovit") {
-            info!("The train is running on time");
-        } else {
-            let late_regex = Regex::new("Kasni . min").unwrap();
+    let appender = tracing_appender::rolling::daily("./logs", "hzpp_delay_stats.log");
+    let (non_blocking_appender, _guard) = tracing_appender::non_blocking(appender);
 
-            let minutes_late: i32 = late_regex
-                .captures(&delay_html)
-                .ok_or_else(|| HzppError::CouldntFindLateness(delay_html.clone()))?[0]
-                .to_owned()
-                .trim()
-                .split(" ")
-                .collect_vec()[1]
-                .to_owned()
-                .parse()?;
+    // A layer that logs events to rolling files.
+    let file_log = tracing_subscriber::fmt::layer()
+        .with_writer(non_blocking_appender)
+        .with_ansi(false)
+        .pretty();
 
-            thread::sleep(Duration::from_millis(25));
-            info!("The train is {} minutes late", minutes_late);
-        }
-    }
-
-    sending_delays_to_db();
-
-    Ok(())
-}
-
-#[instrument]
-fn sending_delays_to_db() {
-    thread::sleep(Duration::from_millis(25));
-}
-
-#[derive(Error, Debug)]
-pub enum HzppError {
-    #[error("Couldn't find how late the train was in the returned html file: {0}")]
-    CouldntFindLateness(String),
-    #[error("Error reading the file {file} | {source}")]
-    FileReadingError {
-        #[source]
-        source: std::io::Error,
-        file: String,
-    },
-    #[error("Error parsing a file")]
-    FileParsingError(#[from] serde_json::Error),
-    #[error("Error parsing the delay duration")]
-    DelayParsingError(#[from] ParseIntError),
+    Registry::default()
+        .with(telemetry_layer)
+        .with(stdout_log)
+        .with(file_log)
+        .with(env_filter)
+        .init();
 }
